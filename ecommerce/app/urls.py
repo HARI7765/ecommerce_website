716b6c8e616f3a893ef4c17fb254b36eabe2d768
harikrishna0001 <nharikrishnanm@gmail.com>
2025-01-30 17:57:18 +0530

    updated

--- ecommerce/app/urls.py
+++ ecommerce/app/urls.py
@@ -1,14 +1,28 @@
-# urls.py
 from django.urls import path
-from . import views
+from . import views  # Import your views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Home page
-    path('signup/', views.signup, name='signup'),  # Signup page
-    path('signin/', views.signin, name='signin'),  # Signin page
-    path('cart/', views.cart, name='cart'),  # Cart page
-    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),  # Add to cart
-    path('add_medicine/', views.add_medicine, name='add_medicine'),  # Add medicine
-    path('add_medical_equipment/', views.add_medical_equipment, name='add_medical_equipment'),  # Add equipment
-    path('admin_page/', views.admin_page, name='admin_page'),  # Admin page
+    # Home page
+    path('', views.index, name='index'),
+
+    # Signup page
+    path('signup/', views.signup, name='signup'),
+
+    # Signin page
+    path('signin/', views.signin, name='signin'),
+
+    # Cart page
+    path('cart/', views.cart, name='cart'),
+
+    # Add to cart (with product ID)
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+
+    # Add medicine (admin-only)
+    path('add_medicine/', views.add_medicine, name='add_medicine'),
+
+    # Add medical equipment (admin-only)
+    path('add_medical_equipment/', views.add_medical_equipment, name='add_medical_equipment'),
+
+    # Admin page
+    path('admin_page/', views.admin_page, name='admin_page'),
 ]
