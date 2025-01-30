db30cb87775f59650479c79ccfe1af823610c8a9
harikrishna0001 <nharikrishnanm@gmail.com>
2025-01-30 17:48:26 +0530

    updated

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/software-11am/Desktop/hari/ecommerce_website/env
+set -gx VIRTUAL_ENV /home/software-4pm/ecommerce_website/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
