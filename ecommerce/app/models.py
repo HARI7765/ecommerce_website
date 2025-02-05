46de6d0af7084870ae79ab0011d4996c3801febb
HARI7765 <nharikrishnanm@gmail.com>
2025-02-05 14:04:50 +0530

    updated

--- ecommerce/app/models.py
+++ ecommerce/app/models.py
@@ -1,41 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    proof = models.FileField(upload_to='proofs/')
 
-    def __str__(self):
-        return self.user.username
-
-
-class Medicine(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='medicines/')
-
-    def __str__(self):
-        return self.name
-
-
-class MedicalEquipment(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='equipment/')
-
-    def __str__(self):
-        return self.name
-from django import forms
-from .models import Medicine, MedicalEquipment
-
-class MedicineForm(forms.ModelForm):
-    class Meta:
-        model = Medicine
-        fields = '__all__'
-
-class MedicalEquipmentForm(forms.ModelForm):
-    class Meta:
-        model = MedicalEquipment
-        fields = '__all__'
+class Product(models.Model):
+    name=models.CharField(max_length=200)
+    price=models.FloatField()
+    description=models.CharField(max_length=500)
+    dosage=models.IntegerField(max_length=200)
--- ecommerce/app/urls.py
+++ ecommerce/app/urls.py
@@ -15,4 +15,13 @@ urlpatterns = [
     path('home/', views.home, name='home'),  # Added 'home' route for proper redirection
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+
+    path('seller/home/', views.seller_view, name='seller'),
+    path('seller/', views.seller_signin_view, name='seller_signin'),
+    path('seller/signup', views.seller_signup_view, name='seller_signup'),
+    path('seller/logout',views.seller_logout_view,name='seller_logout'),
+    path('seller/add',views.seller_add_view,name='seller_add'),
+    path('seller/delete/<int:id>',views.delete_view,name='delete'),
+    path('seller/edit/<int:pk>/', views.edit_view, name='edit'),
 ]
