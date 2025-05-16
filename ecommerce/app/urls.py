8d0eda328fd53f0528f26b539b331e7bf0db6abe
HARI775 <nharikrishnanm@gmail.com>
2025-05-16 16:32:54 +0530

    fixed the views, templates, urls

--- ecommerce/app/urls.py
+++ ecommerce/app/urls.py
@@ -16,14 +16,15 @@ urlpatterns = [
     path('orders/', views.orders_view, name='orders'),
     # Removed the incorrect path with product_details_view
     # Authentication
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
+    path('register/', views.signup, name='register'),
+    path('login/', views.login_view, name='log'),
     path('logout/', views.logout_view, name='logout'),
     
     # Admin features
-    path('ecommerce/register/admin/', views.register_admin_view, name='register_admin'),
     path('ecommerce/dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
     path('ecommerce/add_product/', views.add_product_view, name='add_product'),
+    path('ecommerce/edit_product/<int:id>/', views.edit_product_view, name='edit_product'),
+    path('ecommerce/delete_product/<int:id>/', views.delete_product_view, name='delete_product'),
     path('product_details', views.product_detail_view, name='product_details'),
     path('ecommerce/manage_orders/', views.manage_orders_view, name='manage_orders'),
 
